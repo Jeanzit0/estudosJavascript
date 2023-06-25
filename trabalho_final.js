fd36a0e7ddfc74c5908e150a8868b8c782942d38
Jeanzit0 <136762809+Jeanzit0@users.noreply.github.com>
2023-06-25 10:45:01 -0300

    trabalho_final.js

--- trabalho_final.js
+++ trabalho_final.js
@@ -5,10 +5,14 @@ let cont = 0;
 let multi = 0;
 let soma = 0;
 let vetPrint = Array();
-let vetMulti = Array();
-let multiPrint = 0;
+let vetQnt0 = 0;
+let vetQnt1 = 0;
+let vetQnt2 = 0;
+let vetQnt3 = 0;
+let vetQnt4 = 0;
+let vetQnt5 = 0;
 
-console.log(`\nEspecificação //// Código //// Preço\nCachorro Quente --- 100 --- R$1,20\nBauru Simples --- 101 --- R$1,30\nBauru com Ovo --- 102 --- R$1,50\nHamburguer --- 103 --- R$1,20\nCheeseburguer --- 104 --- R$1,30\nRefrigerante --- 105 --- R$1,00\n`)
+console.log(`\n============  Cardapio  ============\n|| 100 Cachorro quente....R$1,20 ||\n|| 101 Bauru Simples......R$1,30 ||\n|| 102 Bauru com Ovo......R$1,50 ||\n|| 103 Hamburguer.........R$1,20 ||\n|| 104 Cheeseburguer......R$1,30 ||\n|| 105 Refrigerante.......R$1,00 ||\n===================================`)
 
 for(i=1;i>0;i++){
     let code = Number (prompt(`Código do item desejado: `));
@@ -19,43 +23,43 @@ for(i=1;i>0;i++){
             case 100:
                 multi = Number (prompt(`Quantidade: `));
                 cont = vetorPreco[0]*multi;
-                vetMulti[0] = multi; //entra como numero, mostra como string, e precisa voltar como número para cálculo, e ele volta continua como
-                vetPrint[0] = ("cod "+(code + "")+" qnt "+(vetMulti[0] + "")+" R$" +(cont.toFixed(2) + ""));
+                vetQnt0 += multi;
+                vetPrint[0] = ("cod 100 qnt " + `${vetQnt0} R$ ${cont.toFixed(2)}`);
                 soma += cont;
                 break;
             case 101:
-                multi = Number (prompt(`Quantidade:`));
+                multi = Number (prompt(`Quantidade: `));
                 cont = vetorPreco[1]*multi;
-                vetMulti[1] = multi;
-                vetPrint[1] = ("cod "+(code + "")+" qnt "+(vetMulti[1] + "")+" R$" +(cont.toFixed(2) + ""));
+                vetQnt1 += multi;
+                vetPrint[1] = ("cod 101 qnt " + `${vetQnt1} R$ ${cont.toFixed(2)}`);
                 soma += cont;
                 break;
             case 102:
-                multi = Number (prompt(`Quantidade:`));
+                multi = Number (prompt(`Quantidade: `));
                 cont = vetorPreco[2]*multi;
-                vetMulti[2] += multi;
-                vetPrint[2] = ("cod "+(code + "")+" qnt "+(vetMulti[2] + "")+" R$" +(cont.toFixed(2) + ""));
+                vetQnt2 += multi;
+                vetPrint[2] = ("cod 102 qnt " + `${vetQnt2} R$ ${cont.toFixed(2)}`);
                 soma += cont;
                 break;
             case 103:
-                multi = Number (prompt(`Quantidade:`));
+                multi = Number (prompt(`Quantidade: `));
                 cont = vetorPreco[3]*multi;
-                vetMulti[3] += multi;
-                vetPrint[3] = ("cod "+(code + "")+" qnt "+(vetMulti[3] + "")+" R$" +(cont.toFixed(2) + ""));
+                vetQnt3 += multi;
+                vetPrint[3] = ("cod 103 qnt " + `${vetQnt3} R$ ${cont.toFixed(2)}`);
                 soma += cont;
                 break;
             case 104:
-                multi = Number (prompt(`Quantidade:`));
+                multi = Number (prompt(`Quantidade: `));
                 cont = vetorPreco[4]*multi;
-                vetMulti[4] += multi;
-                vetPrint[4] = ("cod "+(code + "")+" qnt "+(vetMulti[4] + "")+" R$" +(cont.toFixed(2) + ""));
+                vetQnt4 += multi;
+                vetPrint[4] = ("cod 104 qnt " + `${vetQnt4} R$ ${cont.toFixed(2)}`);
                 soma += cont;
                 break;
             case 105:
-                multi = Number (prompt(`Quantidade:`));
+                multi = Number (prompt(`Quantidade: `));
                 cont = vetorPreco[5]*multi;
-                vetMulti[5] += multi;
-                vetPrint[5] = ("cod "+(code + "")+" qnt "+(vetMulti[5] + "")+" R$" +(cont.toFixed(2) + ""));
+                vetQnt5 += multi;
+                vetPrint[5] = ("cod 105 qnt " + `${vetQnt5} R$ ${cont.toFixed(2)}`);
                 soma += cont;
                 break;
             default:
